a042c66df989b3e89ba2eab46481a4f3735aa8ed
kunal00000 <kunalverma2468@gmail.com>
2025-02-22 17:08:37 +0530

    feat: implement ArtifactViewer component and update home page layout

---
+++ src/components/artifact.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import React from "react";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarTrigger,
+  useSidebar,
+} from "./ui/sidebar";
+import { cn } from "@/lib/utils";
+
+export default function ArtifactViewer() {
+  const { state } = useSidebar();
+  console.log({ state });
+  return (
+    <div className="flex">
+      <SidebarTrigger
+        className={cn("ml-auto mt-4", state === "expanded" && "opacity-0")}
+      />
+      <Sidebar side="right" className="bg-accent">
+        <section className="ml-2 space-y-2 h-full">
+          <div className="flex gap-4 mt-4 items-center ">
+            <SidebarTrigger />
+            <div className="text-sm font-medium">
+              <p>Browser View</p>
+            </div>
+          </div>
+
+          <SidebarContent className="border-t border-l h-full border-border bg-background rounded-tl-xl">
+            hello
+          </SidebarContent>
+        </section>
+      </Sidebar>
+    </div>
+  );
+}
